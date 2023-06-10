2701c688df01927fc745dcb65604ef406ea1d00b
LoganHX <cosimostabile95@gmail.com>
2023-06-10 20:33:37 +0200

    MOdifica di /area-utente 2

--- src/main/java/it/unisa/c07/biblionet/autenticazione/controller/AreaUtenteController.java
+++ src/main/java/it/unisa/c07/biblionet/autenticazione/controller/AreaUtenteController.java
@@ -10,11 +10,10 @@ import it.unisa.c07.biblionet.utils.BiblionetResponse;
 import it.unisa.c07.biblionet.utils.Utils;
 import it.unisa.c07.biblionet.utils.validazione.RispettoVincoli;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
 
 
@@ -42,84 +41,82 @@ public class AreaUtenteController {
      * da modificare é un esperto.
      * modifica_dati_lettore se l'account
      * da modificare é un lettore.
+     * @RequestMapping(value = "/modifica-dati", method = RequestMethod.GET)
+     * public String modificaDati(final Model model) {
+     * UtenteRegistrato utente = (UtenteRegistrato)
+     * model.getAttribute("loggedUser");
+     * <p>
+     * if (utente != null) {
+     * if (autenticazioneService.isBiblioteca(utente)) {
+     * Biblioteca biblioteca = (Biblioteca) utente;
+     * model.addAttribute("biblioteca", biblioteca);
+     * return "area-utente/modifica-dati-biblioteca";
+     * <p>
+     * } else if (autenticazioneService.isEsperto(utente)) {
+     * Esperto esperto = (Esperto) utente;
+     * model.addAttribute("esperto", esperto);
+     * return "area-utente/modifica-dati-esperto";
+     * <p>
+     * } else if (autenticazioneService.isLettore(utente)) {
+     * Lettore lettore = (Lettore) utente;
+     * model.addAttribute("lettore", lettore);
+     * return "area-utente/modifica-dati-lettore";
+     * <p>
+     * }
+     * }
+     * return "autenticazione/login";
+     * }
+     */
 
-    @RequestMapping(value = "/modifica-dati", method = RequestMethod.GET)
-    public String modificaDati(final Model model) {
-        UtenteRegistrato utente = (UtenteRegistrato)
-                model.getAttribute("loggedUser");
-
-        if (utente != null) {
-            if (autenticazioneService.isBiblioteca(utente)) {
-                Biblioteca biblioteca = (Biblioteca) utente;
-                model.addAttribute("biblioteca", biblioteca);
-                return "area-utente/modifica-dati-biblioteca";
-
-            } else if (autenticazioneService.isEsperto(utente)) {
-                Esperto esperto = (Esperto) utente;
-                model.addAttribute("esperto", esperto);
-                return "area-utente/modifica-dati-esperto";
-
-            } else if (autenticazioneService.isLettore(utente)) {
-                Lettore lettore = (Lettore) utente;
-                model.addAttribute("lettore", lettore);
-                return "area-utente/modifica-dati-lettore";
-
-            }
-        }
-        return "autenticazione/login";
-    }
-*/
-
-    private String controlliPreliminari(BindingResult bindingResult, String vecchia, UtenteRegistrato utenteRegistrato){
-        if(bindingResult.hasErrors()){
+    private String controlliPreliminari(BindingResult bindingResult, String vecchia, UtenteRegistrato utenteRegistrato) {
+        if (bindingResult.hasErrors()) {
             return "Errore di validazione";
         }
 
-        if(autenticazioneService.login(utenteRegistrato.getEmail(), vecchia) == null){ //usata solo per vedere se la password vecchia corrisponde, non effettua davvero il login
+        if (autenticazioneService.login(utenteRegistrato.getEmail(), vecchia) == null) { //usata solo per vedere se la password vecchia corrisponde, non effettua davvero il login
             return "Password errata. Non sei autorizzato a modificare la password.";
         }
 
         return "";
 
     }
-    private String qualePassword(String vecchia, String nuova, String conferma){
-        if(nuova.isEmpty() && conferma.isEmpty()) return vecchia;
+
+    private String qualePassword(String vecchia, String nuova, String conferma) {
+        if (nuova.isEmpty() && conferma.isEmpty()) return vecchia;
         if (RispettoVincoli.confrontoPassword(nuova, conferma)) {
             return conferma;
         }
         return vecchia;
     }
+
     /**
      * Implementa la funzionalità di modifica dati di una biblioteca.
      *
      * @param biblioteca email della biblioteca da modificare.
-     * @param vecchia La vecchia password dell'account.
-     * @param nuova La nuova password dell'account.
-     * @param conferma La password di conferma password dell'account.
-     *
+     * @param vecchia    La vecchia password dell'account.
+     * @param nuova      La nuova password dell'account.
+     * @param conferma   La password di conferma password dell'account.
      * @return login Se la modifica va a buon fine.
      * modifica_dati_biblioteca Se la modifica non va a buon fine
-     *
      */
     @PostMapping(value = "/conferma-modifica-biblioteca")
     @ResponseBody
     @CrossOrigin
     public BiblionetResponse modificaDatiBiblioteca(
-            final @RequestHeader (name="Authorization") String token,
+            final @RequestHeader(name = "Authorization") String token,
             final @Valid @RequestParam("Biblioteca") Biblioteca biblioteca,
             BindingResult bindingResult,
-            final @RequestParam("vecchia_password")String vecchia,
-            final @RequestParam("nuova_password")String nuova,
-            final @RequestParam("conferma_password")String conferma) {
+            final @RequestParam("vecchia_password") String vecchia,
+            final @RequestParam("nuova_password") String nuova,
+            final @RequestParam("conferma_password") String conferma) {
 
         Claims claims = Utils.getClaimsFromTokenWithoutKey(token);
-        if(! claims.getSubject().equalsIgnoreCase(biblioteca.getEmail()))
+        if (!claims.getSubject().equalsIgnoreCase(biblioteca.getEmail()))
             return new BiblionetResponse("Non sei autorizzato", false);
 
         biblioteca.setPassword(qualePassword(vecchia, nuova, conferma));
         String s = controlliPreliminari(bindingResult, vecchia, biblioteca);
-        if(!s.isEmpty()) return new BiblionetResponse("Non sei autorizzato", false);
-
+        if (!s.isEmpty()) return new BiblionetResponse("Non sei autorizzato", false);
 
 
         autenticazioneService.aggiornaBiblioteca(biblioteca);
@@ -128,16 +125,14 @@ public class AreaUtenteController {
     }
 
 
-
     /**
      * Implementa la funzionalità di modifica dati di un esperto.
      *
-     * @param esperto Un esperto da modificare.
-     * @param vecchia La vecchia password dell'account.
-     * @param nuova La nuova password dell'account.
-     * @param conferma La password di conferma password dell'account.
+     * @param esperto         Un esperto da modificare.
+     * @param vecchia         La vecchia password dell'account.
+     * @param nuova           La nuova password dell'account.
+     * @param conferma        La password di conferma password dell'account.
      * @param emailBiblioteca L'email della biblioteca scelta.
-     *
      * @return login Se la modifica va a buon fine.
      * modifica_dati_esperto Se la modifica non va a buon fine
      */
@@ -145,24 +140,23 @@ public class AreaUtenteController {
     @ResponseBody
     @CrossOrigin
     public BiblionetResponse modificaDatiEsperto(
-            final @RequestHeader (name="Authorization") String token,
+            final @RequestHeader(name = "Authorization") String token,
             final @Valid @RequestParam("Esperto") Esperto esperto,
             BindingResult bindingResult,
-            final @RequestParam("vecchia_password")String vecchia,
-            final @RequestParam("nuova_password")String nuova,
-            final @RequestParam("conferma_password")String conferma,
-            final @RequestParam("email_biblioteca")String emailBiblioteca) {
-
+            final @RequestParam("vecchia_password") String vecchia,
+            final @RequestParam("nuova_password") String nuova,
+            final @RequestParam("conferma_password") String conferma,
+            final @RequestParam("email_biblioteca") String emailBiblioteca) {
 
 
         Esperto toUpdate = autenticazioneService.findEspertoByEmail(esperto.getEmail());
         Claims claims = Utils.getClaimsFromTokenWithoutKey(token);
-        if(! claims.getSubject().equalsIgnoreCase(esperto.getEmail()))
+        if (!claims.getSubject().equalsIgnoreCase(esperto.getEmail()))
             return new BiblionetResponse("Non sei autorizzato", false);
 
         esperto.setPassword(qualePassword(vecchia, nuova, conferma));
         String s = controlliPreliminari(bindingResult, vecchia, esperto);
-        if(!s.isEmpty()) new BiblionetResponse(s, false);
+        if (!s.isEmpty()) new BiblionetResponse(s, false);
 
 
         Biblioteca b = autenticazioneService.findBibliotecaByEmail(emailBiblioteca);
@@ -180,31 +174,30 @@ public class AreaUtenteController {
     /**
      * Implementa la funzionalità di modifica dati di un lettore.
      *
-     * @param lettore Un lettore da modificare.
-     * @param vecchia La vecchia password dell'account.
-     * @param nuova La nuova password dell'account.
+     * @param lettore  Un lettore da modificare.
+     * @param vecchia  La vecchia password dell'account.
+     * @param nuova    La nuova password dell'account.
      * @param conferma La password di conferma password dell'account.
-     *
      * @return login Se la modifica va a buon fine.
      * modifica_dati_lettore Se la modifica non va a buon fine
      */
     @RequestMapping(value = "/conferma-modifica-lettore",
             method = RequestMethod.POST)
-    public BiblionetResponse confermaModificaLettore(final @RequestHeader (name="Authorization") String token,
-                     final @Valid @ModelAttribute Lettore lettore,
-                     BindingResult bindingResult,
-                     final @RequestParam("vecchia_password")String vecchia,
-                     final @RequestParam("nuova_password")String nuova,
-                     final @RequestParam("conferma_password")String conferma) {
+    public BiblionetResponse confermaModificaLettore(final @RequestHeader(name = "Authorization") String token,
+                                                     final @Valid @ModelAttribute Lettore lettore,
+                                                     BindingResult bindingResult,
+                                                     final @RequestParam("vecchia_password") String vecchia,
+                                                     final @RequestParam("nuova_password") String nuova,
+                                                     final @RequestParam("conferma_password") String conferma) {
 
 
         Claims claims = Utils.getClaimsFromTokenWithoutKey(token);
         //if(! claims.getSubject().equalsIgnoreCase(lettore.getEmail()))
-          //  return new ResponseEntity<>("Non sei autorizzato", HttpStatus.FORBIDDEN);
+        //  return new ResponseEntity<>("Non sei autorizzato", HttpStatus.FORBIDDEN);
 
         lettore.setPassword(qualePassword(vecchia, nuova, conferma));
         String s = controlliPreliminari(bindingResult, vecchia, lettore);
-        if(!s.isEmpty()) return new BiblionetResponse(s, false);
+        if (!s.isEmpty()) return new BiblionetResponse(s, false);
 
 
         autenticazioneService.aggiornaLettore(lettore);
@@ -217,11 +210,13 @@ public class AreaUtenteController {
      * in base al tipo.
      *
      * @return La view di visualizzazione area utente
-    */
-    @RequestMapping(value = "/area-utente", method = RequestMethod.GET)
+     */
+    @GetMapping(value = "/area-utente")
+    @ResponseBody
+    @CrossOrigin
     public UtenteRegistrato datiAreaUtente(
-            @RequestHeader (name="Authorization") final String token
-            ) {
+            @RequestHeader(name = "Authorization") final String token
+    ) {
         {
             if (Utils.isUtenteBiblioteca(token)) {
                 return autenticazioneService.findBibliotecaByEmail(Utils.getSubjectFromToken(token));
@@ -229,7 +224,6 @@ public class AreaUtenteController {
                 return autenticazioneService.findEspertoByEmail(Utils.getSubjectFromToken(token));
             } else if (Utils.isUtenteLettore(token)) {
                 return autenticazioneService.findLettoreByEmail(Utils.getSubjectFromToken(token));
-
             }
         }
         return null;
@@ -242,18 +236,18 @@ public class AreaUtenteController {
      * @param model Utilizzato per gestire la sessione.
      * @return La view di visualizzazione dei clubs a cui é iscritto
 
-    @RequestMapping(value = "area-utente/visualizza-clubs-personali-lettore",
-            method = RequestMethod.GET)
-    public String visualizzaClubsLettore(final Model model) {
-        Lettore utente = (Lettore) model.getAttribute("loggedUser");
-        if (utente != null && autenticazioneService.isLettore(utente)) {
-            model.addAttribute("clubs",
-                    autenticazioneService.findAllByLettori(utente));
-            return "area-utente/visualizza-clubs-personali";
-        }
-        return "autenticazione/login";
-    }
-    */
+     @RequestMapping(value = "area-utente/visualizza-clubs-personali-lettore",
+     method = RequestMethod.GET)
+     public String visualizzaClubsLettore(final Model model) {
+     Lettore utente = (Lettore) model.getAttribute("loggedUser");
+     if (utente != null && autenticazioneService.isLettore(utente)) {
+     model.addAttribute("clubs",
+     autenticazioneService.findAllByLettori(utente));
+     return "area-utente/visualizza-clubs-personali";
+     }
+     return "autenticazione/login";
+     }
+     */
 
     /**
      * Implementa la funzionalità di visualizzazione dei clubs
@@ -261,16 +255,16 @@ public class AreaUtenteController {
      * @param model Utilizzato per gestire la sessione.
      * @return La view di visualizzazione dei clubs che gestisce
 
-    @RequestMapping(value = "area-utente/visualizza-clubs-personali-esperto",
-            method = RequestMethod.GET)
-    public String visualizzaClubsEsperto(final Model model) {
-        Esperto utente = (Esperto) model.getAttribute("loggedUser");
-        if (utente != null && autenticazioneService.isEsperto(utente)) {
-            model.addAttribute("clubs",
-                    autenticazioneService.findAllByEsperto(utente));
-            return "area-utente/visualizza-clubs-personali";
-        }
-        return "autenticazione/login";
-    }
-    */
+     @RequestMapping(value = "area-utente/visualizza-clubs-personali-esperto",
+     method = RequestMethod.GET)
+     public String visualizzaClubsEsperto(final Model model) {
+     Esperto utente = (Esperto) model.getAttribute("loggedUser");
+     if (utente != null && autenticazioneService.isEsperto(utente)) {
+     model.addAttribute("clubs",
+     autenticazioneService.findAllByEsperto(utente));
+     return "area-utente/visualizza-clubs-personali";
+     }
+     return "autenticazione/login";
+     }
+     */
 }
