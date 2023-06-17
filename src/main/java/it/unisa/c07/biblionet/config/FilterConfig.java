6280eb200e5d96227cbcd23a88d02a95d04e27b4
LoganHX <omisoc95@gmail.com>
2023-06-17 10:25:21 +0200

    Migliorata granularità securizzazione

--- src/main/java/it/unisa/c07/biblionet/config/FilterConfig.java
+++ src/main/java/it/unisa/c07/biblionet/config/FilterConfig.java
@@ -13,7 +13,15 @@ public class FilterConfig {
         FilterRegistrationBean filter = new FilterRegistrationBean();
         filter.setFilter(new JwtFilter());
 
-        filter.addUrlPatterns("/biblioteca/*");
+        filter.addUrlPatterns("/biblioteca/inserimento-isbn");
+        filter.addUrlPatterns("/biblioteca/inserimento-archivio");
+        filter.addUrlPatterns("/biblioteca/inserimento-manuale");
+
+        filter.addUrlPatterns("/prenotazione-libri/conferma-prenotazione");
+        filter.addUrlPatterns("/prenotazione-libri/visualizza-richieste");
+        filter.addUrlPatterns("/prenotazione-libri/visualizza-prenotazioni");
+
+
 
         return filter;
     }
